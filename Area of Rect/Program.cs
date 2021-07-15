795b7d195f276c4b3cf376f90fdf294e77612797
Prajakta <Prajakta0709@gmail.com>
2021-07-14 22:28:32 -0500

    Add project files.

---
+++ Area of Rect/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Area_of_Rect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            string inputX;
+            string inputY;
+
+            Console.WriteLine("Enter length of rectangle: ");
+            inputX = Console.ReadLine();
+            x = double.Parse(inputX);
+
+            Console.WriteLine("Enter width of rectangle: ");
+           inputY = Console.ReadLine();
+            y = double.Parse(inputY);
+
+            Console.WriteLine("Area of rectangle : " + x * y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Miles per gal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Miles_per_gal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of miles driven: ");     
+            double miles = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the gas consumed: ");
+            double gas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Miles per gallon : " + miles/gas + "mpg");
+            Console.ReadLine();
+        }
+    }
+}
